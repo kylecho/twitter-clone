f7fe87f9bb0b7372f0fdf2873c2a87221c35b888
seaspi <sea.spi@gmail.com>
2014-03-18 02:35:59 -0700

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
-class StaticPagesController < ApplicationController
-  def home
+class StaticPagesController < ApplicationController # Classes are simply a convenient way to organize methods(functions)
+  def home # method
   end
 
   def help
+  end
+
+  def about
   end
 end
