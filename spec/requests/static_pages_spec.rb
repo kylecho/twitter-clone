3b894c4124fa84d01cfb5d51f33d6c49ceaa9d6b
seaspi <sea.spi@gmail.com>
2014-03-18 02:51:06 -0700

    Finished an exercise 3.5.2 to refactorize titles in the spec file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,6 +4,8 @@ describe "Static pages" do
 
   describe "Home page" do
 
+    let(:base_title) { "SNS는 모다?" }
+
     it "should have the content 'SNS'" do
       visit '/static_pages/home'
       expect(page).to have_content('SNS')
@@ -11,7 +13,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("SNS는 모다? | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -24,7 +26,7 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("SNS는 모다? | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -37,7 +39,7 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("SNS는 모다? | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
 
@@ -50,7 +52,7 @@ describe "Static pages" do
 
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_title("SNS는 모다? | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
