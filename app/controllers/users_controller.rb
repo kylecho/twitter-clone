6c722a396da296e14aae35fdab13cd587c2454fd
seaspi <sea.spi@gmail.com>
2014-03-19 09:14:58 -0700

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "'SNS는 모다?'에 오신것을 환영합니다!"
       redirect_to @user
     else
