0746d3baa9fac48a40350103c0f4b3293b7c862b
seaspi <sea.spi@gmail.com>
2014-03-19 16:19:47 -0700

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController # Classes are simply a convenient way to organize methods(functions)
-  def home # method
+  def home
+    if signed_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
